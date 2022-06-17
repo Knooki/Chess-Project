79ac035307c84df42fd191d51ab2e1ec32dd372c
Knooki <knukknakiknak@gmail.com>
2022-06-17 18:58:35 +0300

    Made base api. first serializers

--- backend/applications/quickstart/serializers.py
+++ backend/applications/quickstart/serializers.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class UserSericalizer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Userfields = ['url', 'usernmae', 'email', 'groups']
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = Groupfields = ['url', 'name']
+        model = Group
+        fields = ['url', 'name']
