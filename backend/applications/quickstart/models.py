112615d47f608a4e4095f162a806cfb12f2543c3
Knooki <knukknakiknak@gmail.com>
2022-06-17 20:12:26 +0300

    django-countries added

--- backend/applications/quickstart/models.py
+++ backend/applications/quickstart/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_countries.fields import CountryField
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    country = CountryField()
